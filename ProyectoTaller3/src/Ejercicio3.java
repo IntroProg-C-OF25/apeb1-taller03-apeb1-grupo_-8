c582dca2788b59acc1a0f5096e73d69687422bcb
andresvallejoT-T <andresvallejo102006@gmail.com>
2024-10-24 12:15:55 -0500

    Ejercicio 3

--- ProyectoTaller3/src/Ejercicio3.java
+++ ProyectoTaller3/src/Ejercicio3.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Andrés
- */
 public class Ejercicio3 {
-    
+    public static void main(String[] args) {
+        double respuesta;
+        respuesta =  Math.pow(10,2) + Math.sqrt(16) +(20)/(1+11);
+         System.out.println("respuesta = " + respuesta);
+    }
 }
