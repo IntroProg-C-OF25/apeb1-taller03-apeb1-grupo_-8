cd8b7870c25caae83c421e56f0c359fd9031da62
andresvallejoT-T <andresvallejo102006@gmail.com>
2024-10-24 12:01:41 -0500

    Ejercicio 1

--- ProyectoTaller3/src/Ejercicio1.java
+++ ProyectoTaller3/src/Ejercicio1.java
@@ -1,12 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Andrés
- */
 public class Ejercicio1 {
-    
+    public static void main(String[] args) {
+        double respuesta;
+        respuesta = ((1+2+3+4)/5) + Math.sqrt(81);
+        System.out.println("respuesta = " + respuesta);
+    }
 }
