4096bf484a34cf99bfbcf893e8424fd1b75afbbc
Jorandres13 <jaguerrero39@utpl.edu.ec>
2024-10-24 12:33:17 -0500

    Ejercicio 4

--- ProyectoTaller3/src/Ejercicio4.java
+++ ProyectoTaller3/src/Ejercicio4.java
@@ -8,5 +8,13 @@
  * @author Andrés
  */
 public class Ejercicio4 {
+    public static void main(String[] args) {
+        boolean respuesta;
+        respuesta = (20/(1+3))+2 >= 3;
+        System.out.println("respuesta = " + respuesta);
+                
+                
+
+    }
     
 }
