57293d78042f7f35f03c828a6fd25375c7c7a61d
Jorandres13 <jaguerrero39@utpl.edu.ec>
2024-10-24 13:39:44 -0500

    Ejercicio 9

--- ProyectoTaller3/src/Ejercicio9.java
+++ ProyectoTaller3/src/Ejercicio9.java
@@ -1,8 +1,10 @@
 
 public class Ejercicio9 {
-
+    
     public static void main(String[] args) {
-        bool
+        boolean respuesta;
+        respuesta = Math.sqrt(81) + Math.sqrt(25) * 10 >= 50;
+        System.out.println("respuesta = " + respuesta);
     }
-
+    
 }
