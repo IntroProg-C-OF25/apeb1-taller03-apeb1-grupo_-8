fe8599284d3bf5c98fc3ec40b8c985b760b28ffc
Jorandres13 <jaguerrero39@utpl.edu.ec>
2024-10-24 12:55:08 -0500

    Ejercicio 8

--- ProyectoTaller3/src/Ejercicio8.java
+++ ProyectoTaller3/src/Ejercicio8.java
@@ -8,5 +8,11 @@
  * @author Andrés
  */
 public class Ejercicio8 {
+    public static void main(String[] args) {
+        boolean respuesta;
+        respuesta = ((10*9)+20-(500/2)+Math.pow(3, 2)>=200) || (50>=50-50+51);
+        System.out.println("respuesta = " + respuesta);
+                
+    }
     
 }
