57f5b9fb837047562f88ab67aefaffb4b34dd58d
Jorandres13 <jaguerrero39@utpl.edu.ec>
2024-10-24 12:44:55 -0500

    Ejercicio 6

--- ProyectoTaller3/src/Ejercicio6.java
+++ ProyectoTaller3/src/Ejercicio6.java
@@ -8,5 +8,10 @@
  * @author Andrés
  */
 public class Ejercicio6 {
-    
+    public static void main(String[] args) {
+        boolean respuesta;
+        respuesta = (((Math.sqrt(81) + 9) / 3)== 9)&& (10>1);
+        System.out.println("respuesta = " + respuesta);
+                
+    }
 }
