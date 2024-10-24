3f9f05e703743816f9f9bc8aaf734565a121b558
Jorandres13 <jaguerrero39@utpl.edu.ec>
2024-10-24 12:18:11 -0500

    Ejercicio 2

--- ProyectoTaller3/src/Ejercicio2.java
+++ ProyectoTaller3/src/Ejercicio2.java
@@ -8,5 +8,11 @@
  * @author Andrés
  */
 public class Ejercicio2 {
+    public static void main(String[] args) {
+        double respuesta;
+        respuesta = Math.pow(10,2) + 10;
+        System.out.println("respuesta = " + respuesta);
+                
+    }
     
 }
